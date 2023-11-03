d3ab6ee3d790a7224ee504237fd87caa9806f18c
JEvarMarro <jose.evaristo.marro@gmail.com>
2023-11-02 23:47:59 -0300

    Se agrega autenticatión de usuarios

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'rack-cors', require: 'rack/cors'
 
 gem 'active_model_serializers'
 
+# Authentication 
+gem 'devise-jwt'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :card_conditions
   resources :auction_items
   resources :cards
@@ -10,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, skip: %i[registrations sessions passwords]
+  devise_scope :user do
+    post '/signup', to: 'registrations#create'
+    post '/login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+  end
 end
